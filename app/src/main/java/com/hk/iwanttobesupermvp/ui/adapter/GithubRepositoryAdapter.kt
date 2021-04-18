e4011c0b46aea6fae50ef24ace63479cd6f8328b
SSong-develop <thdgnsrl123@naver.com>
2021-04-18 13:13:48 +0900

    [feat] : delegate 공부를 해보았다~

--- app/src/main/java/com/hk/iwanttobesupermvp/ui/adapter/GithubRepositoryAdapter.kt
+++ app/src/main/java/com/hk/iwanttobesupermvp/ui/adapter/GithubRepositoryAdapter.kt
@@ -13,8 +13,6 @@ import kotlin.reflect.KProperty
 class GithubRepositoryAdapter :
     RecyclerView.Adapter<GithubRepositoryAdapter.GithubRepositoryViewHolder>() {
 
-    // 이러한 형태도 있다! 라고 알아두시면 좋을거 같습니다
-    // 위임 패턴 (Delegate Pattern) 이라는 것을 사용했습니다.
     var mockRepositoryList: List<MockDataEntity> by Delegates.observable(emptyList()) { _, oldValue, newValue ->
         if (oldValue != newValue) notifyDataSetChanged()
     }
